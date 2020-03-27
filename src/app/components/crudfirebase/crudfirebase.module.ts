bc0e1466566be48dc368e57c17569282cf6620de
edsvalen <edsvalen@bancolombia.com.co>
2020-03-27 16:14:38 -0500

    organizar modulos

--- src/app/components/crudfirebase/crudfirebase.module.ts
+++ src/app/components/crudfirebase/crudfirebase.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeroeComponent } from './heroe/heroe.component';
-import { HeroesComponent } from './heroes/heroes.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import {NgbPaginationModule} from '@ng-bootstrap/ng-bootstrap';
+import { HeroeComponent } from './heroe/heroe.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
 
 
 
