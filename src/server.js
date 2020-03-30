e9cffc6743440a43c35c363d6b085f6dab89dbf8
edsvalen <edsvalen@bancolombia.com.co>
2020-03-30 03:16:30 -0500

    update port

--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@ const path = require('path');
 
 const app = express();
 
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 5000);
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/Angular9'));
 
@@ -12,5 +15,3 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/Angular9/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 5000);
