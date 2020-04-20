e7987a9dc9b7cb32229fb623fff0ce8039cecfc6
edsvalen <edsvalen@bancolombia.com.co>
2020-04-20 15:25:08 -0500

    style color y fondo

--- src/app/components/dyrcocinas/cotizacion-ferreteria/cotizacion-ferreteria.component.ts
+++ src/app/components/dyrcocinas/cotizacion-ferreteria/cotizacion-ferreteria.component.ts
@@ -1,32 +1,70 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { SelectionModel } from '@angular/cdk/collections';
 
-export interface Transaction {
-  item: string;
-  cost: number;
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
 }
+
 
 @Component({
   selector: 'app-cotizacion-ferreteria',
   templateUrl: './cotizacion-ferreteria.component.html',
   styleUrls: ['./cotizacion-ferreteria.component.css']
 })
+
+
+
 export class CotizacionFerreteriaComponent implements OnInit {
-  displayedColumns = ['item', 'cost'];
-  transactions: Transaction[] = [
-    {item: 'Beach ball', cost: 4},
-    {item: 'Towel', cost: 5},
-    {item: 'Frisbee', cost: 2},
-    {item: 'Sunscreen', cost: 4},
-    {item: 'Cooler', cost: 25},
-    {item: 'Swim suit', cost: 15},
+ 
+   ELEMENT_DATA: PeriodicElement[] = [
+    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
   ];
+  displayedColumns: string[] = [ 'position', 'name'];
+  dataSource = new MatTableDataSource<PeriodicElement>(this.ELEMENT_DATA);
+  selection = new SelectionModel<PeriodicElement>(true, []);
   constructor() { }
 
   ngOnInit(): void {
   }
 
   getTotalCost() {
-    return this.transactions.map(t => t.cost).reduce((acc, value) => acc + value, 0);
+    return this.ELEMENT_DATA.map(t => t.position).reduce((acc, value) => acc + value, 0);
+  }
+
+
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+        this.selection.clear() :
+        this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: PeriodicElement): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
   }
 }
+
 
